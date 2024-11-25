6b0c36a46bb4b3fb439fd3fd2614de016ea65472
Yotam Greenstein <yotamgr314@gmail.com>
2024-11-25 22:42:57 +0200

    aadsasd
    adasd

--- client/src/components/user.list.js
+++ client/src/components/user.list.js
@@ -1,4 +1,4 @@
-/*NOTE: The components folder contains reusable React components that represent UI elements or features. 
+/*NOTE: DONT KNOW WHAT THIS IS !! NOT SURE - The components folder contains reusable React components that represent UI elements or features. 
 This file (user.list.js) is responsible for rendering the list of users, and handling UI logic such as 
 iterating over users, displaying their data, and optionally interacting with user-related hooks or services.
 */
