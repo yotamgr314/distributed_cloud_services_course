09df0d6b67e1f282cfe8231f004b336a7001bddb
Yotam Greenstein <yotamgr314@gmail.com>
2024-11-26 23:55:31 +0200

    asdas
    asdas

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,7 +3,7 @@ import User from '../models/user.model.js';
 /* Create new user */
 export const createUser = async (req, res) => {
     try {
-        const newUser = await User.create(req.body); // `User.create` will also save the user to the MongoDB.
+        const newUser = await User.create(req.body); // NOTE - create() is a built in mongoose function which creates an instance of document and saves it to MongoDB, it returns a promise.
         res.status(201).json(newUser); // Respond with the saved user
 
     }catch (err) {
@@ -19,19 +19,17 @@ export const createUser = async (req, res) => {
 
 export const getUsers = async (req, res) => {
     try {
-        const users = await User.find({}); // Find all users in the database
-        if (!users) {
-            res.status(404).json({ message: 'No users found' });
-        }
+        const users = await User.find(); // NOTE - find() is a built in mongoose function that returns all the users documents in the mongoDB, it returns a promise.
 
-        if(users.length === 0){
-            res.status(204).json({ message: 'empty response' });
+        if (users.length === 0) {
+            return res.status(200).json([]); // Return an empty array with a 200 status
         }
 
         res.status(200).json(users); // Send back the list of users
 
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        res.status(500).json({ message: err.message }); // Handle server errors
     }
 };
+
 
