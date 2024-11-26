a0713abd48d9ec0485448b0dc97d0b39575fafb3
Yotam Greenstein <yotamgr314@gmail.com>
2024-11-26 23:50:31 +0200

    asdas
    dasdas

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,16 +1,19 @@
 import User from '../models/user.model.js';
 
-// Create a new user
+/* Create new user */
 export const createUser = async (req, res) => {
     try {
+        const newUser = await User.create(req.body); // `User.create` will also save the user to the MongoDB.
+        res.status(201).json(newUser); // Respond with the saved user
 
-        const user = new User(req.body); // Create a new user from the request body
-        const savedUser = await user.save(); // Save the user to the database
-        res.status(201).json(savedUser); // Send back the saved user
-    } catch (err) {
-        res.status(400).json({ message: err.message });
+    }catch (err) {
+
+        res.status(400).json({
+            message: err.message
+        });
     }
 };
+
 
 // Get all users
 
@@ -24,7 +27,9 @@ export const getUsers = async (req, res) => {
         if(users.length === 0){
             res.status(204).json({ message: 'empty response' });
         }
+
         res.status(200).json(users); // Send back the list of users
+
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
