3daab390614c90114cb01c8627d5be438b9ac72b
Yotam Greenstein <yotamgr314@gmail.com>
2024-12-02 17:34:53 +0200

    asds
    asd

--- server/app.js
+++ server/app.js
@@ -1,28 +1,38 @@
+//IMPORTS SECTION 
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-
-
 import userRoutes from './routes/user.routes.js';
 
-// Load environment variables from .env file
-dotenv.config();
+//CONFIGURATION SECTION
+dotenv.config(); /*Load environment variables from .env file  */ 
 
-// Connect to MongoDB
-connectDB();
 
-const app = express();
+connectDB(); /*NOTE: calls a function which establish the connection to our mongoDB database */
 
-// Middleware
-app.use(cors());
-app.use(express.json()); //TODO  find out what this does
+const app = express(); /*NOTE calls express constructor, via its instance we will gain access to many express framework methods related to creating a web app,
+                         for example: 
+                         restfull API request routes such as : app.get/POST/DELETE/PUT 
+                         NOTE app.use() - to define Middlewares such as                 
+*/
 
-// Routes
+
+//MIDDLEWARES SECTION
+app.use(cors()); /* NOTE :CORS is a security mechanism implemented by browsers to restrict how resources on a web page can be requested from another domain. 
+                    By default, browsers block requests made to a server from a different origin (protocol, domain, or port) than the one hosting the web page.
+                    NOTE for example:
+                    If your frontend is deployed one domain e.g., Render and my backend (Express server) is deployed on another domain - e.g. my college server, the browser will block requests made to your API unless CORS is enabled.
+                */
+                    
+app.use(express.json()); // NOTE This middleware parses incoming JSON payloads from the request body and makes them accessible via req.body. It's essential for handling POST or PUT requests where the client sends data in JSON format.
+
+
+
+//ROUTES SECTION
 app.use('/api/users', userRoutes);
-
-// Default route
-app.get('/', (req, res) => {
+ 
+app.get('/', (req, res) => { // Default route - must always be the last route.
     res.send('Welcome to the Distributed Cloud Services API');
 });
 
